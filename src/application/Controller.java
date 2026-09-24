cf71db9b6189880a0a042cb5e634399a288123f0
BlackSunVirus <josuechenel1@gmail.com>
2026-09-24 13:59:01 +0200

    Ajout début controller & main

---
+++ src/application/Controller.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Scanner;
+
+public class Controller {
+    public static Scanner scan = new Scanner(System.in);
+
+    // Affiche le menu principal
+    public void afficherMenu() {
+        System.out.println("===== GESTION DE LA DISCOTHÈQUE =====");
+        System.out.println("1. Ajouter un album");
+        System.out.println("2. Lister tous les albums");
+        System.out.println("3. Rechercher un album");
+        System.out.println("4. Supprimer un album");
+        System.out.println("0. Quitter");
+    }
+
+    public void ajouterAlbum() {
+
+    }
+
+    public void afficherDiscotheque() {
+
+    }
+
+    public void afficherAlbum() {
+
+    }
+
+    public void supprimerAlbum() {
+
+    }
+}
