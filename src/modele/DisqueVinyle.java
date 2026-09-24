8114b4d0c59f6b1015f55598ae927a3bf220b2ab
Romain <rcamenen@gmail.com>
2026-09-24 12:35:48 +0200

    Ajout class fille DisqueVinyle

--- src/modele/DisqueVinyle.java
+++ src/modele/DisqueVinyle.java
@@ -1,4 +1,43 @@
 package modele;
 
-public class DisqueVinyle {
+import java.time.LocalDate;
+
+public class DisqueVinyle extends Album {
+
+     private String numero;
+     private int taille;
+
+     public DisqueVinyle(String nom, String auteur, LocalDate date, int quantite, String numero, int taille) {
+          super(nom, auteur, date, quantite);
+          this.numero = numero;
+          this.taille = taille;
+     }
+
+     public String getNumero() {
+          return numero;
+     }
+
+     public int getTaille() {
+          return taille;
+     }
+
+     public void setNumero(String numero) {
+          this.numero = numero;
+     }
+
+     public void setTaille(int taille) {
+          this.taille = taille;
+     }
+
+     @Override
+     public String getSupport(){
+          return "Disque vinyle";
+     }
+
+     @Override
+     public String toString() {
+          return super.toString()
+                  +", numero = " + numero
+                  +", taille = " + taille;
+     }
 }
